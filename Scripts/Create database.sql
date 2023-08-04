de508c8797e21603a6447c3f95faae82f7102565
MVMmaksM <vaganov.maksim94@yandex.ru>
2023-08-04 10:13:39 +0500

    Refacroring

--- Scripts/Create database.sql
+++ Scripts/Create database.sql
@@ -16,54 +16,28 @@ CREATE DATABASE PasswordRespondent
 GO
 USE PasswordRespondent
 GO
-CREATE TABLE [dbo].[sector]
-(
-	[id] INT IDENTITY(1,1) NOT NULL,
-	[name] NVARCHAR(25) NOT NULL,
-
-	CONSTRAINT PK__sector_id PRIMARY KEY CLUSTERED([id]),
-	CONSTRAINT UQ__sector_name UNIQUE([name]) 
-)
-GO
-CREATE TABLE [dbo].[role]
-(
-	[id] INT IDENTITY(1,1) NOT NULL,
-	[name] NVARCHAR(25) NOT NULL,
-
-	CONSTRAINT PK__role_id PRIMARY KEY CLUSTERED([id]),
-	CONSTRAINT UQ__role_name UNIQUE([name])
-)
-GO
 CREATE TABLE [dbo].[user]
 (
 	[id] INT IDENTITY(1,1) NOT NULL,
-	[login] NVARCHAR(255) NOT NULL,
-	[password] NVARCHAR(25) NOT NULL,
-	[date_create] DATE DEFAULT GETDATE(),
-	[sector_id] INT NOT NULL,
-	[role_id] INT NOT NULL
+	[name] NVARCHAR(24) NOT NULL
+
 
 	CONSTRAINT PK__user_id PRIMARY KEY CLUSTERED ([id]),
-	CONSTRAINT UQ__user_login UNIQUE ([login]),
-	CONSTRAINT FK__user_sector_id FOREIGN KEY ([sector_id]) REFERENCES [dbo].[sector]([id]),
-	CONSTRAINT FK__user_role_id FOREIGN KEY([role_id]) REFERENCES [dbo].[role]([id])
+	CONSTRAINT UQ__user_name UNIQUE ([name])
 )
 GO
-CREATE TABLE [dbo].[respondent]
+CREATE TABLE [dbo].[respondent_asgs]
 (
-	[id] INT IDENTITY(1,1) NOT NULL,
-	[okpo] NVARCHAR(15) NOT NULL,
+	[id] INT IDENTITY(1,1) NOT NULL,	
 	[full_name] NVARCHAR(MAX) NOT NULL,
 	[short_name] NVARCHAR(255) NOT NULL,
-	[okato_fact] BIGINT NOT NULL,
-	[okved2007_base] NVARCHAR(25) NULL,
-	[okved2_base] NVARCHAR(25) NOT NULL,
-	[adress] NVARCHAR(MAX) NOT NULL,
-	[telephone] NVARCHAR(64) NULL,
-	[email_contact] NVARCHAR(255) NULL,
-	[email_asgs] NVARCHAR(255) NULL,
+	[okpo] NVARCHAR(15) NOT NULL,
+	[okpo_ul] NVARCHAR(15) NOT NULL,
 	[inn] BIGINT NOT NULL,
-	[ogrn] BIGINT NOT NULL,
+	[adress] NVARCHAR(255) NOT NULL,
+	[telephone] NVARCHAR(64) NULL,
+	[dop_telephone]
+	
 
 	CONSTRAINT PK__respondent_id PRIMARY KEY CLUSTERED([id]),
 	CONSTRAINT UQ__respondent_okpo UNIQUE([okpo])
